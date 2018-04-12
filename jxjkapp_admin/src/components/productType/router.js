052730f1bed9b366361e0c495106d99fc00f581c
zhengjitf <zhengji1991@gmail.com>
2018-04-12 22:02:39 +0800

    admin: 修改路由配置文件

--- jxjkapp_admin/src/components/productType/router.js
+++ jxjkapp_admin/src/components/productType/router.js
@@ -1,12 +1,8 @@
-import injectPermissionId from '@/utils/injectPermissionId'
 import {
   INDEX
 } from './_consts/routers'
-// 该模块的权限ID
-const permissionId = INDEX.meta.permissionId
 
-export default injectPermissionId([
-  Object.assign({}, INDEX, {
-    component: resolve => require(['./Index'], resolve)
-  })
-], permissionId)
+export default [{
+  ...INDEX,
+  component: require('./Index')
+}]
